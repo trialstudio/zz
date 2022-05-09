065389aa49082dcbfe30f6d0ed88d11f06a1107a
ibacangan <i.bacangan@gmail.com>
2022-05-10 02:38:58 +0800

    test

--- scripts/vars/main.groovy
+++ scripts/vars/main.groovy
@@ -6,26 +6,21 @@ def initialize() {
             templateEngine.createTemplate("${libraryResource template}").make(bindings).toString()
         }
 
-    pipeline {
-        agent { label 'built-in' }
-        stages {
-            stage('Create jobs') {
-                def teamApps = readYaml text: "${libraryResource 'team-apps.yaml'}"
+    node('built-in') {
+        def teamApps = readYaml text: "${libraryResource 'team-apps.yaml'}"
 
-                teamApps.each { team ->
-                    team.apps.each { app ->
-                        def defaultBindings = ["team": team.name, "app": app.name]
-                        switch (app.type) {
-                            case "springboot": addSpringbootPipelines(app.name, renderer, defaultBindings)
-                                break
-                            default: echo 'type not found'
-                        }
-                    }
-                    addCategorizedViewJobDsl(team.name,
-                            "^(${team.apps.collect { it.name }.join('|')})-(build|deploy-to-dev|deploy-to-prod)",
-                            "^(.*)-(build|deploy-to-dev|deploy-to-prod)")
+        teamApps.each { team ->
+            team.apps.each { app ->
+                def defaultBindings = ["team": team.name, "app": app.name]
+                switch(app.type) {
+                    case "springboot": addSpringbootPipelines(app.name, renderer, defaultBindings)
+                        break
+                    default: echo 'type not found'
                 }
             }
+            addCategorizedViewJobDsl(team.name,
+                    "^(${team.apps.collect { it.name }.join('|')})-(build|deploy-to-dev|deploy-to-prod)",
+                    "^(.*)-(build|deploy-to-dev|deploy-to-prod)")
         }
     }
 }
