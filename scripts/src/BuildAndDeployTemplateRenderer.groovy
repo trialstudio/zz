4bca487c6eef1d0838c93677d784ea322c47137e
ibacangan <i.bacangan@gmail.com>
2022-05-10 00:24:20 +0800

    test

--- scripts/src/BuildAndDeployTemplateRenderer.groovy
+++ scripts/src/BuildAndDeployTemplateRenderer.groovy
@@ -4,19 +4,23 @@ class BuildAndDeployTemplateRenderer {
     private HashMap<String, String> environmentTemplateMap
     private String buildTemplate
     private static SimpleTemplateEngine engine = new SimpleTemplateEngine()
+    private Closure templateContentRetriever
 
-    BuildAndDeployTemplateRenderer(String buildTemplate, HashMap<String, String> environmentTemplateMap) {
+    BuildAndDeployTemplateRenderer(String buildTemplate, HashMap<String, String> environmentTemplateMap, Closure templateContentRetriever) {
+        this.templateContentRetriever = templateContentRetriever
         this.buildTemplate = buildTemplate
         this.environmentTemplateMap = environmentTemplateMap
     }
 
     String renderBuildTemplate(HashMap<String, String> bindings) {
-        return engine.createTemplate("${libraryResource buildTemplate}").make(bindings).toString()
+//        return engine.createTemplate("${libraryResource buildTemplate}").make(bindings).toString()
+        return engine.createTemplate(templateContentRetriever(buildTemplate)).make(bindings).toString()
     }
 
     List<HashMap<String, String>> renderEnvironmentTemplate(HashMap<String, String> bindings) {
         return environmentTemplateMap.collect {
-            return [(it.key): engine.createTemplate("${libraryResource it.value}").make(bindings + ["env": it.key]).toString()]
+//            return [(it.key): engine.createTemplate("${libraryResource it.value}").make(bindings + ["env": it.key]).toString()]
+            return [(it.key): engine.createTemplate(templateContentRetriever(it.value)).make(bindings + ["env": it.key]).toString()]
         }
     }
 }
